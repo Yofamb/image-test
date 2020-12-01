0d7c2bf71bf902c1283f8a24acd8481690d7c549
Ben Kannenberg <yofamb@gmail.com>
2020-12-01 16:32:18 +1100

    Test images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,43 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+import { Flex, AspectRatio, Box } from "theme-ui"
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+export default ({ data }) => (
+  <div>
+    <Flex>
+      <Box sx={{width: 300, border: "1px solid black"}}>
+        <AspectRatio ration={1}>
+          <Img fixed={data.file.childImageSharp.fixed} style={{height: "100%"}} imgStyle={{ objectFit: "contain", objectPosition: "bottom center", width: "100%", height: "100%" }} />
+        </AspectRatio>
+      </Box>
+      <Box sx={{width: 50}}></Box>
+      <Box sx={{width: 300, border: "1px solid black"}}>
+        <AspectRatio ration={1}>
+          <Img fluid={data.file.childImageSharp.fluid} style={{ height: "100%" }} imgStyle={{ objectFit: "contain", objectPosition: "bottom center", width: "100%", height: "100%"}} />
+        </AspectRatio>
+      </Box>
+      <Box sx={{ width: 50 }}></Box>
+      <Box sx={{ width: 300, border: "1px solid black" }}>
+        <AspectRatio ration={1}>
+          <Img fluid={data.file.childImageSharp.fluid} style={{ height: "100%" }} imgStyle={{ objectFit: "contain", objectPosition: "bottom center", top: "auto", bottom: "0", width: "100%", height: "125%" }} />
+        </AspectRatio>
+      </Box>
+    </Flex>
+  </div>
+)
+
+export const query = graphql`
+  query {
+    file(relativePath: { eq: "bottle-standard.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+        fixed {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`
